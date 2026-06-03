ce637c17214c2fa9657aae23ec2476473fb3e2c4
go-arnold <arnoldciku@gmail.com>
2026-06-03 12:01:22 +0200

    fix: attenting to fix failed ci checks

--- backend/apps/authentication/views/logout.py
+++ backend/apps/authentication/views/logout.py
@@ -5,7 +5,6 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from ..serializers import LogoutSerializer
 
